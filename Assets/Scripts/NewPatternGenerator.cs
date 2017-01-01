cdb79e48be86f1619cbfba0e7bd20a3f326b714b
LNWPOR <tclnwpor@gmail.com>
2017-01-01 16:37:09 +0700

    now can generate the Pattern

---
+++ Assets/Scripts/NewPatternGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPatternGenerator : MonoBehaviour {
+
+    public GameObject[] patterns;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            int nextPatternIndex = (int)Mathf.Round(Random.Range(0, patterns.Length));
+            Transform stage = transform.parent.parent;
+            Transform currentPattern = transform.parent;
+            Transform currentPlane = currentPattern.GetChild(0);
+            Vector3 newPatternPoint = new Vector3(0, 0, currentPlane.GetComponent<MeshRenderer>().bounds.size.z + currentPlane.position.z);
+            GameObject newGeneratePattern = Instantiate(patterns[nextPatternIndex], newPatternPoint, Quaternion.identity) as GameObject;
+            newGeneratePattern.transform.parent = stage;
+            newGeneratePattern.name = patterns[nextPatternIndex].gameObject.name;
+            if (!currentPattern.Equals(stage.GetChild(0)))
+            {
+                Destroy(stage.GetChild(0).gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StageController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour {
+
+    public int currentStageNumber;
+
+    void Start () {
+        currentStageNumber = 1;
+	}
+
+    public void ChangeStage(int stageNumber)
+    {
+        currentStageNumber = stageNumber;
+        GameObject currentStageGenerator = transform.GetChild(stageNumber - 1).gameObject;
+        currentStageGenerator.SetActive(true);
+    }
+	
+}
