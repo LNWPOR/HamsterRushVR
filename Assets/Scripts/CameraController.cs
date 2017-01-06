ff930ffb85b7c3e3bddb1914d3ac96cd8d09700b
LNWPOR <tclnwpor@gmail.com>
2017-01-07 01:04:29 +0700

    add CameraMoveIn

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    private Animator anim;
+    void Awake(){
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Start () {
+        anim.Play("CameraMoveIn");
+    }
+	
+}
