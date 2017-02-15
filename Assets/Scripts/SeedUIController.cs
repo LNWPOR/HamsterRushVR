3149672931fa889ca4db472062f2f0c65ee3460f
LNWPOR <tclnwpor@gmail.com>
2017-02-15 21:53:11 +0700

    add cartoonui package, add hamsteranim, fix ultimate to seed

---
+++ Assets/Scripts/SeedUIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeedUIController : MonoBehaviour {
+
+    public Text seedText;
+    private GameObject player;
+    private PlayerUltimateController playerUltimateControllerScript;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	void Start () {
+        playerUltimateControllerScript = player.GetComponent<PlayerUltimateController>();
+	}
+	void Update () {
+        seedText.text = "Seeds : " + playerUltimateControllerScript.currentUltiPoint;	
+	}
+}
