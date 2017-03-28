66d70fec51791020d464900371b91c51f51f5446
LNWPOR <tclnwpor@gmail.com>
2017-03-29 02:43:28 +0700

    add collider to all envi and add Destroyable tag to them

--- Assets/Scripts/SwordController.cs
+++ Assets/Scripts/SwordController.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class SwordController : WeaponController {
-   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Destroyable"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 
 }
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class WeaponController : MonoBehaviour
 {
     public Transform holdingPos;
+
+    
 }
