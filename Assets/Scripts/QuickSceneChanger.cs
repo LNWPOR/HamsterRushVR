b29399f554c4b0f650a707684e31e40965a17afb
LNWPOR <tclnwpor@gmail.com>
2017-03-29 00:07:11 +0700

    add QuickSceneChanger

---
+++ Assets/Scripts/QuickSceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class QuickSceneChanger : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetKeyDown("1"))
+        {
+            SceneManager.LoadScene("PreStart");
+        }else if (Input.GetKeyDown("2"))
+        {
+            SceneManager.LoadScene("Menu");
+        }else if (Input.GetKeyDown("3"))
+        {
+            SceneManager.LoadScene("GamePlay");
+        }
+	}
+}
