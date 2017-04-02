101c0fea1fc9684bc538761a9569ef6fc1e7a7af
LNWPOR <tclnwpor@gmail.com>
2017-04-02 22:40:41 +0700

    add handModel in menu scene

---
+++ Assets/Scripts/HandPoseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPoseController : MonoBehaviour {
+    public GameObject handExtend;
+    public GameObject handHold;
+    public GameObject capsuleHand;
+    private HandExtend handExtendScript;
+    private HandHold handHoldScript;
+    private void Awake()
+    {
+        handExtendScript = capsuleHand.GetComponent<HandExtend>();
+        handHoldScript = capsuleHand.GetComponent<HandHold>();
+    }
+    private void Update()
+    {
+        if (handHoldScript.handIsHold.Equals(true))
+        {
+            handHold.SetActive(true);
+            handExtend.SetActive(false);
+        }
+        else
+        {
+            handExtend.SetActive(true);
+            handHold.SetActive(false);
+        }
+    }
+}
