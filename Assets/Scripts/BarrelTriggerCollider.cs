7f29b692d1a7077c0cb3625cb3c80c0625a229c4
LNWPOR <tclnwpor@gmail.com>
2017-04-13 02:44:08 +0700

    add Stage3

---
+++ Assets/Scripts/BarrelTriggerCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelTriggerCollider : MonoBehaviour {
+    public float barrelSpeed;
+    public GameObject barrel;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            barrel.AddComponent<Rigidbody>();
+            Rigidbody barrelRigidbody = barrel.GetComponent<Rigidbody>();
+            barrelRigidbody.constraints =   RigidbodyConstraints.FreezePositionX|
+                                            RigidbodyConstraints.FreezeRotationZ;
+            barrelRigidbody.AddForce(new Vector3( 0,0, -barrelSpeed));
+        }
+    }
+}
---
+++ Assets/Scripts/barrelHitCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrelHitCollider : MonoBehaviour {
+
+    public int damage = 1;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            GameObject player = other.gameObject;
+            PlayerSeedController playerSeedControllerScript = player.GetComponent<PlayerSeedController>();
+            playerSeedControllerScript.DecreaseSeed(damage);
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
