8286ab6df07b675be2df5dac5af00fa14d58e4d0
LNWPOR <tclnwpor@gmail.com>
2017-04-13 02:46:19 +0700

    detect empty name

--- Assets/Scripts/ReadyButton.cs
+++ Assets/Scripts/ReadyButton.cs
@@ -13,40 +13,42 @@ public class ReadyButton : MonoBehaviour {
     public void OnClickReady()
     {
         //Debug.Log(playerName.text);
-
-        var webAddr = GameManager.Instance.URL + "/api/players";
-        var req = (HttpWebRequest)WebRequest.Create(webAddr);
-        req.ContentType = "application/json; charset=utf-8";
-        req.Method = "POST";
-
-        using (var streamWriter = new StreamWriter(req.GetRequestStream()))
+        if (playerName.text.Length > 0)
         {
-            JSONObject data = new JSONObject();
-            data.AddField("name",playerName.text);
-            streamWriter.Write(data.ToString());
-            streamWriter.Flush();
-        }
+            var webAddr = GameManager.Instance.URL + "/api/players";
+            var req = (HttpWebRequest)WebRequest.Create(webAddr);
+            req.ContentType = "application/json; charset=utf-8";
+            req.Method = "POST";
 
-        var response = (HttpWebResponse)req.GetResponse();
-        using (var streamReader = new StreamReader(response.GetResponseStream()))
-        {
-            JSONObject result = new JSONObject(streamReader.ReadToEnd());
-            if (result.GetField("status").ToString().Equals("1"))
+            using (var streamWriter = new StreamWriter(req.GetRequestStream()))
             {
-                //Debug.Log("Sign in success.");
-                string id = Converter.JsonToString(result.GetField("player").GetField("_id").ToString());
-                string name = Converter.JsonToString(result.GetField("player").GetField("name").ToString());
-                float scores = Converter.JsonToFloat(result.GetField("player").GetField("scores").ToString());
-                float seeds = Converter.JsonToFloat(result.GetField("player").GetField("seeds").ToString());
-                GameManager.Instance.playerData = new PlayerData(id, name, scores, seeds);
-                SceneManager.LoadScene("Menu");
+                JSONObject data = new JSONObject();
+                data.AddField("name", playerName.text);
+                streamWriter.Write(data.ToString());
+                streamWriter.Flush();
             }
-            else 
+
+            var response = (HttpWebResponse)req.GetResponse();
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
             {
-                //error
-                Debug.Log(result);
+                JSONObject result = new JSONObject(streamReader.ReadToEnd());
+                if (result.GetField("status").ToString().Equals("1"))
+                {
+                    //Debug.Log("Sign in success.");
+                    string id = Converter.JsonToString(result.GetField("player").GetField("_id").ToString());
+                    string name = Converter.JsonToString(result.GetField("player").GetField("name").ToString());
+                    float scores = Converter.JsonToFloat(result.GetField("player").GetField("scores").ToString());
+                    float seeds = Converter.JsonToFloat(result.GetField("player").GetField("seeds").ToString());
+                    GameManager.Instance.playerData = new PlayerData(id, name, scores, seeds);
+                    SceneManager.LoadScene("Menu");
+                }
+                else
+                {
+                    //error
+                    Debug.Log(result);
+                }
+
             }
-            
         }
     }
 }
