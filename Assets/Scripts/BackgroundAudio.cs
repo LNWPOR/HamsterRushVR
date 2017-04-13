3a74aa6e4f3be47cbc013cd7a4948e2dbd9b01e8
LNWPOR <tclnwpor@gmail.com>
2017-04-13 21:07:19 +0700

    add background Audio

---
+++ Assets/Scripts/BackgroundAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        GameObject backgroundAudioInUse = GameObject.Find("BackgroundAudioInUse");
+        if (!object.Equals(backgroundAudioInUse,null))
+        {
+            Destroy(gameObject);
+        }else
+        {
+            gameObject.name = "BackgroundAudioInUse";
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
